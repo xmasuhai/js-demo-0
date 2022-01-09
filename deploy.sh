ad8723390c6869f0a431699ea0b819293286c20c
xmasuhai <xmasuhai@163.com>
2022-01-10 06:03:19 +0800

    👷 🚀 部署

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+rm -rf dist &&
+
+# 生成静态文件
+yarn play &&
+
+# 进入生成的文件夹
+cd ./dist &&
+
+git init &&
+git add -A &&
+git commit -m 'deploy' &&
+
+git push -f git@github.com:xmasuhai/js-demo-0.git master:gh-pages &&
+
+# git remote add origin https://gitee.com/xmasuhai/vue-demo-1.git &&
+git push -u origin master
+cd -
---
+++ dist/index.e381625f.js
@@ -0,0 +1,40 @@
+const div1 = document.createElement('div');
+div1.className = 'demo';
+document.body.appendChild(div1);
+let dragging = false;
+let lastX;
+let lastY;
+// 按下鼠标 记录 鼠标位置 clientX clientY
+div1.onmousedown = (e)=>{
+    lastX = e.clientX;
+    lastY = e.clientY;
+    dragging = true;
+};
+document.onmousemove = (e)=>{
+    if (dragging === true) {
+        // 记录移动后鼠标位置坐标的差值
+        const deltaX = e.clientX - lastX;
+        const deltaY = e.clientY - lastY;
+        // 设定 元素的移动量 element.style.top element.style.left
+        const left = parseInt(div1.style.left, 10) || 0;
+        const top = parseInt(div1.style.top, 10) || 0;
+        // 阻止用户把div搞不见，即位置X Y的数值不低于零
+        const resultX = left + deltaX;
+        const resultY = top + deltaY;
+        // 设定 元素的定位样式
+        div1.style.left = `${resultX}px`;
+        div1.style.top = `${resultY}px`;
+        // 更新 lastX lastY 的位置
+        lastX = e.clientX;
+        lastY = e.clientY;
+    /*
+    console.log(['e.clientX:', e.clientX, 'e.clientY:', e.clientY])
+    console.log([deltaX, deltaY, lastX, lastY])
+    */ }
+};
+div1.onmouseup = ()=>{
+    dragging = false;
+} // 不放在document.body上
+;
+
+//# sourceMappingURL=index.e381625f.js.map
